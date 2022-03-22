0ae11622497924d7693aa77f4812ff0599b5bd95
Garv Khurana <gk12345garvkhurana@gmail.com>
2022-03-22 20:50:57 +0530

    sign in page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,14 +1,3 @@
-/*!
-=========================================================
-* Muse Ant Design Dashboard - v1.0.0
-=========================================================
-* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-* Coded by Creative Tim
-=========================================================
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import React, { Component } from "react";
 import {
   Layout,
@@ -130,43 +119,12 @@ export default class SignUp extends Component {
             <div className="header-col header-brand">
               <h5>Muse Dashboard</h5>
             </div>
-            <div className="header-col header-nav">
-              <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
-                <Menu.Item key="1">
-                  <Link to="/dashboard">
-                    {template}
-                    <span> Dashboard</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Link to="/profile">
-                    {profile}
-                    <span>Profile</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Link to="/sign-up">
-                    {signup}
-                    <span> Sign Up</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="4">
-                  <Link to="/sign-in">
-                    {signin}
-                    <span> Sign In</span>
-                  </Link>
-                </Menu.Item>
-              </Menu>
-            </div>
-            <div className="header-col header-btn">
-              <Button type="false">FREE DOWNLOAD</Button>
-            </div>
           </Header>
 
           <Content className="p-0">
             <div className="sign-up-header">
               <div className="content">
-                <Title>Sign Up</Title>
+                <Title>Sign In</Title>
                 <p className="text-lg">
                   Use these awesome forms to login or create new account in your
                   project for free.
@@ -199,14 +157,6 @@ export default class SignUp extends Component {
                 className="row-col"
               >
                 <Form.Item
-                  name="Name"
-                  rules={[
-                    { required: true, message: "Please input your username!" },
-                  ]}
-                >
-                  <Input placeholder="Name" />
-                </Form.Item>
-                <Form.Item
                   name="email"
                   rules={[
                     { required: true, message: "Please input your email!" },
@@ -223,22 +173,13 @@ export default class SignUp extends Component {
                   <Input placeholder="Passwoed" />
                 </Form.Item>
 
-                <Form.Item name="remember" valuePropName="checked">
-                  <Checkbox>
-                    I agree the{" "}
-                    <a href="#pablo" className="font-bold text-dark">
-                      Terms and Conditions
-                    </a>
-                  </Checkbox>
-                </Form.Item>
-
                 <Form.Item>
                   <Button
                     style={{ width: "100%" }}
                     type="primary"
                     htmlType="submit"
                   >
-                    SIGN UP
+                    SIGN IN
                   </Button>
                 </Form.Item>
               </Form>
